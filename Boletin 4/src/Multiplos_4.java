b27ba6565dc21444dbe00c7dbc12fe16db9daa34
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-24 20:37:34 +0200

    Update Multiplos_4.java

--- Boletin 4/src/Multiplos_4.java
+++ Boletin 4/src/Multiplos_4.java
@@ -1,9 +1,10 @@
-public class Multiplos_4 {
+public class Multiplos_Boletin4 {
     public static void main(String[] args){
 /*Bucle que repite procesos a partir de unas condiciones */
         for(int a=1; a<=100;a++){
             /*Salida de multiplos de 7*/
             System.out.println(a*7);
+            /*Salida de datos q son multiplos de los que me dieron*/
         }
 
 

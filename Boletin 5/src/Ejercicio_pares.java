d73fef5d145a655b7ccb447efb36b7b05e730ca9
Dima <dapariciopereira@danielcastelao.org>
2024-10-30 08:43:22 +0100

    Ejercicos 5 resueltos

---
+++ Boletin 5/src/Ejercicio_pares.java
@@ -0,0 +1,31 @@
+public class Ejercicio_pares {
+    public static void main(String[] args){
+
+        int suma=0;
+        for(int i=0;i<10;i++){
+            if (i%2==0)  suma=suma+i;
+            else continue;
+            System.out.println("El numero es:  "+i);
+            }
+        System.out.println("La suma de los pares es: "+suma);
+
+
+        }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+

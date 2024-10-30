5d936cfce4ebf7522acefe1c22d2e1796288afed
Dima <dapariciopereira@danielcastelao.org>
2024-10-30 09:54:30 +0100

    Ejercicos 5_7 resuelto

--- Boletin 5/src/Boletin_5_7.java
+++ Boletin 5/src/Boletin_5_7.java
@@ -4,14 +4,29 @@ public class Boletin_5_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1;
-        int n2;
-        System.out.println("Dame un numero: ");
-        n1 = sc.nextInt();
+        int sum=0;
 
-      do{
-          String i =" * ";
-          System.out.println(i+"\n");
-      }while (n1>=n1+1);
 
-        }
+
+
+           for(int i=1;i<10;i++) {
+
+               do {
+                   System.out.println("Dame un numero: ");
+                   n1 = sc.nextInt();
+                   sum=sum+n1;
+                   if (n1>=999){
+                       System.out.println(sum);
+                       break;
+                   }else{
+                       break;
+                   }
+               }while (i++<10);
+
+
+           }
+
+
+
+    }
 }

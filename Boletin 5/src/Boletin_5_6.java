cca40c09992d9a6eeb1bc960e3d5570d1129bb7c
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-31 23:39:09 -1200

    Update Boletin_5_6.java

--- Boletin 5/src/Boletin_5_6.java
+++ Boletin 5/src/Boletin_5_6.java
@@ -9,33 +9,23 @@ import java.util.Scanner;
 public class Boletin_5_6 {
     public static void main(String[] args){
 
-Scanner sc=new Scanner(System.in);
-int n1;
-System.out.println("Dame un numero: ");
-n1=sc.nextInt();
-int f=1;
-for (int i = 1; i<=n1; i++) {
-    f=f*i;
+    Scanner sc=new Scanner(System.in);
+    int n1;   
+        /*Pedimos un valor numerico para que lo esrciba en consola*/
+        
+        System.out.println("Dame un numero: ");
+        n1=sc.nextInt();
+        int f=1;
+        
+        for (int i = 1; i<=n1; i++) {
+            /*Creamos una funcion de repeticion para que nos de numeros para su posterior uso*/
+            f=f*i;
+            /*operacion del calculo factorial*/
 
-}
+            }
 System.out.println(f);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+        /*Salida del resultado*/
+       
+    } 
+    sc.close;
 }

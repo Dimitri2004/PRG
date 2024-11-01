071a15a95313fc0f78c9eb4e6e5d16494548bff4
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-31 23:34:18 -1200

    Update Boletin_5_8.java

--- Boletin 5/src/Boletin_5_8.java
+++ Boletin 5/src/Boletin_5_8.java
@@ -9,28 +9,23 @@ public class Boletin_5_8{
         Scanner sc = new Scanner(System.in);
         int n1;
         int sum=0;
-
-
-
-
+        /*Damos valores int a nuestras nuevas variables  */
            for(int i=1;i<10;i++) {
-
+/*Creamos una fuuncion de repeticion que nos de numeros para su futuro uso*/
                do {
+                   /*Creamos un bucle en el que repita los procesos de pedir numeros por cansola y crear operacion de suma en pantalla*/
                    System.out.println("Dame un numero: ");
                    n1 = sc.nextInt();
                    sum=sum+n1;
                    if (n1>=999){
+                       /*Limitamos los numeros escritos por consola ya q solo nos sirven los que sean menores de 999*/
                        System.out.println(sum);
                        break;
                    }else{
                        break;
                    }
+                   /*hacer q el bucle se repita las diez primeras veces*/
                }while (i++<10);
-
-
            }
-
-
-
     }
 }

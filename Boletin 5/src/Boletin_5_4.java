6591602aef2af27a384c5f969f360472971cd2cf
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-31 23:43:15 -1200

    Update Boletin_5_4.java

--- Boletin 5/src/Boletin_5_4.java
+++ Boletin 5/src/Boletin_5_4.java
@@ -10,16 +10,15 @@ public class Boletin_5_4 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n1;
+        /*Preguntamos el numero por consola para saber su multiplicidad */
         System.out.println("Dame un numero: ");
         n1=sc.nextInt();
+        /*La salida de datos de abajo es para que se vea de forma ordenada y clara lo que el programa realiza*/
         System.out.println("La tabla del "+n1+" es: ");
         for(int i=0;i<=10;i++){
+            /*Crea una cadena de numeros para su posterior uso*/
             System.out.println(n1+"x"+i+"="+n1*i);
-
+            /*Salida de los numeros con su repectiva tabla de multiplicar*/
         }
-
-
-
-
     }
 }

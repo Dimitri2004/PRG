ea0b93925de08f61f2f40a807898c8c5d8abac69
Dima <dapariciopereira@danielcastelao.org>
2024-11-11 14:41:26 +0100

    Boletin_7 ejercicios resueltos

--- Boletin_7/src/Boletin_7_2.java
+++ Boletin_7/src/Boletin_7_2.java
@@ -1,2 +1,39 @@
+
+import java.util.Random;
+
+import static java.lang.Long.sum;
+
 public class Boletin_7_2 {
-}
+    public static void main(String[] args) {
+        notas();
+    }
+
+    static void notas() {
+        int[] nota = new int[30];
+        for (int i = 1; i < nota.length; i++) {
+            nota[i] = (int) Math.floor(Math.random() * 10) + 1;
+
+            if (nota[i] < 5) {
+                System.out.println("Suspenso =" + nota[i]);
+            } else if (nota[i] == 10) {
+                System.out.println("La maxima nota = " + nota[i]);
+            } else {
+                System.out.println("Aprobado = " + nota[i]);
+            }
+        }
+            int ciclo = 0, num = 0, res = 0;
+            Random aleatorio = new Random();
+            String imprimir = "Suma de N numeros aleatorios: \n";
+
+            for (int i = 0; i < nota.length; i++) {
+                nota[i] = (int) Math.floor(Math.random() * 10) + 1;
+                num = nota[i];
+                imprimir = imprimir + num + "\n";
+                res = res + num;
+            }
+
+            System.out.println(res);
+        }
+
+    }
+

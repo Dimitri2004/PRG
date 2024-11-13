6935c8f6f01556315e0848a1fa834573ef3985fa
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-11-13 03:35:01 -1200

    Update BOLETIN_7_1.java

--- BOLETIN_7/src/BOLETIN_7_1.java
+++ BOLETIN_7/src/BOLETIN_7_1.java
@@ -1,17 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+*@author Dima Aparicio
+*@version 1.0
+**/
 public class BOLETIN_7_1 {
 
         public static void main(String[] args) {
-            int[] num = new int[6];
+            int[] num = new int[6];/**crea un array de 6 numeros**/
 
             int j;
             for(j = 0; j < num.length; ++j) {
-                num[j] = (int)Math.floor(Math.random() * 51.0);
+                num[j] = (int)Math.floor(Math.random() * 51.0);/**genera numeros aleatorios enterso entre 0 y 50**/
             }
 
             for(j = num.length - 1; j > 0; --j) {
-                System.out.println(num[j]);
+                System.out.println(num[j]);/**lee los numeros generados del anterior del reves**/
             }
 
         }

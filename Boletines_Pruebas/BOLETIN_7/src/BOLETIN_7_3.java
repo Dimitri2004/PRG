16811773efc5453cc592deeab766ced32719afd5
Dima <dapariciopereira@danielcastelao.org>
2024-11-20 09:31:28 +0100

    Boletin 7_3 resuelto, y comentado

--- Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_3.java
+++ Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_3.java
@@ -1,19 +1,20 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+/**Crear comentrarios para cada linea**/
 
 public class BOLETIN_7_3 {
-    public static void main(String[] args) {
-        int[] nota = notas();
-        String[] alum = {
+    public static void main(String[] args) { //TODO
+        int[] nota = notas();//notas
+        String[] alum = {//alumnos
                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Z", "Ad", "Bo", "Ca", "De", "Ga", "Lu"
         };
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el nombre del alumno: ");
+        Scanner scanner = new Scanner(System.in);//Scanner para leer de la consola
+        System.out.print("Ingrese el nombre del alumno: ");//salida por pantalla
         String nombreAlumno = scanner.nextLine();
-
+        //Obtener la nota del alumno
         int resultado = obtenerNotaPorNombre(nombreAlumno, alum, nota);
+        //Si se encontró al alumno
         if (resultado != -1) {
             System.out.println("La nota del alumno " + nombreAlumno + " es: " + resultado);
         } else {
@@ -22,13 +23,13 @@ public class BOLETIN_7_3 {
 
         scanner.close();
     }
-
+//Generar las notas y las ordenar de mayor a menor
     static int[] notas() {
         int[] nota = new int[30]; // crea un array de 30 numeros
         String[] alum = {
                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Z", "Ad", "Bo", "Ca", "De", "Ga", "Lu"
         };
-
+        //Notas aleatorias y ordenarlas
         for (int i = 0; i < nota.length; i++) {
             nota[i] = (int) (Math.random() * 10) + 1; // crea numeros aleatorios entre 1 y 10
         }
@@ -41,7 +42,7 @@ public class BOLETIN_7_3 {
             nota[nota.length - 1 - i] = temp;
         }
 
-        // Imprimir los resultados
+        // Condicionar los alumnos que aprobaron y obtuvieron la maxima nota
         for (int i = 0; i < nota.length; i++) {
             if (nota[i] >= 5) {
                 System.out.println("Alumno " + alum[i] + " aprobo con " + nota[i]);
@@ -50,10 +51,11 @@ public class BOLETIN_7_3 {
                 System.out.println("Alumno " + alum[i] + " obtuvo la maxima nota " + nota[i]);
             }
         }
-
         return nota;
     }
+    // Buscar la nota del alumno por nombre
     static int obtenerNotaPorNombre(String nombre, String[] alum, int[] nota) {
+        // Recorrer el array de alumnos y comparar con el nombre del alumno
         for (int i = 0; i < alum.length; i++) {
             if (alum[i].equals(nombre)) {
                 return nota[i];

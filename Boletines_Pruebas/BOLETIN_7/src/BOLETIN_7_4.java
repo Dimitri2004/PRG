5fcdb0da697040eab9b2e05c49618de3ec9d5f5a
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-11-22 04:40:38 -1200

    Update BOLETIN_7_4.java

--- Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_4.java
+++ Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_4.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 
 public class BOLETIN_7_4 {
     public static void main(String[] args) {
-        alumno_dni();
+        alumno_dni();//Salida del resultado en forma de funcion en el main
     }
 
-    static void alumno_dni() {
+    static void alumno_dni() {//Creamos la funcion que va a realizar el proceso de selecccion de letra
         Scanner sc = new Scanner(System.in);
-
+//Creamos y preguntamos las variables que va a necesitar
         int dni;
         System.out.println("Dame el numero de tu DNI: ");
         dni=sc.nextInt();
         int [] num =new int[9];
         int res;
         res = dni% 23;
+        //Creamos el array correspondiente a las letras que utilizan los DNIs
         String[] letra = {"T", "R", "W", "A", "G ", "M ", "Y", "F", "P", "D", "X",
                 "B", "N", "J", "Z ", "S ", "Q", "V", " H ", "L ", "C ", "K", "E"};
         for (int i = 0; i < 1; i++) {

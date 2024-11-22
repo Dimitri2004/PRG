78d3f941134862b6ef7ba6b6c58bf6e4d86c193a
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-11-22 05:10:04 -1200

    Update BOLETIN_7_4.java

--- Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_4.java
+++ Boletines_Pruebas/BOLETIN_7/src/BOLETIN_7_4.java
@@ -18,7 +18,7 @@ public class BOLETIN_7_4 {
         //Creamos el array correspondiente a las letras que utilizan los DNIs
         String[] letra = {"T", "R", "W", "A", "G ", "M ", "Y", "F", "P", "D", "X",
                 "B", "N", "J", "Z ", "S ", "Q", "V", " H ", "L ", "C ", "K", "E"};
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 1; i++) {//Entramos dentro del array generando una unica salida de letra
             if (num.length != 8) {
                 System.out.println("Al "+dni+" le corresonde la "+letra[(res)]);
             } else {

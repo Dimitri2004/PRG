dff2318b71fbabb9a2e85b5d462349b27dda9d28
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-11-25 04:00:12 -1200

    Update Boletin_7_7.java

--- Boletines_Pruebas/BOLETIN_7/src/Boletin_7_7.java
+++ Boletines_Pruebas/BOLETIN_7/src/Boletin_7_7.java
@@ -1,14 +1,17 @@
 public class Boletin_7_7 {
     public static void main(String[] args) {
+        //Creamos el array 
         int[] lista = new int[10];
         for (int i = 0; i < lista.length; i++) {
             lista[i] = i*2;
         }
+        //Creamos nuestra lista de valoras a copiar
         int [] nova_lista=copia_lista(lista);
         for (int elemento:nova_lista) {
             System.out.print(elemento+ " ");
         }
     }
+    //Creamos la funcion que realizara la copia del array anterior
     public static int[] copia_lista(int[] lista_a_copiar) {
         int[] nova_lista;
         nova_lista = new int[lista_a_copiar.length];

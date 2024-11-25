3440cdf61cf98947b3d408ffe50080b0b557bda0
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-11-25 04:47:20 -1200

    Update Boletin_7_8.java

--- Boletines_Pruebas/BOLETIN_7/src/Boletin_7_8.java
+++ Boletines_Pruebas/BOLETIN_7/src/Boletin_7_8.java
@@ -1,12 +1,16 @@
 public class Boletin_7_8 {
     public static void main(String[] args) {
+        //Creamos el array de numeros que decidira si es par el array de la funcion igualada
         int[] numero=Numeros();
+        System.out.println("Son pares: ")
+            // Creamos la condicion de entrada al array de numeros
        for(int numeros:numero) {
            if (numeros % 2 == 0) {
-               System.out.println("\nSon pares "+numeros);
+               System.out.print(numeros);
            }
        }
     }
+    //Funcion donde crearemos el array de numeros aleatorios
     static int[] Numeros() {
         int[] numero=new int[9];
         for (int i = 0; i <numero.length; i++) {

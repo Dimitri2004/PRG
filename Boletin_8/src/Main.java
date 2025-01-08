3e23c75a591cbe72145691ede1a0059e3ce6b2f8
Dima <dapariciopereira@danielcastelao.org>
2025-01-08 08:51:07 +0100

    PRG

--- Boletin_8/src/Main.java
+++ Boletin_8/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        //Crear un lector de strings da palabra "java java java"
+        String palabra="java\bjava\bjava";
+        System.out.println(palabra.length());
+
 
     }
 

1a878ee7e0aedd019c94624448cce567bc709045
Dima <dapariciopereira@danielcastelao.org>
2025-01-08 10:11:56 +0100

    PRG/Boletin 8/Terminado

--- Boletines_Pruebas/Boletin_8/src/Main.java
+++ Boletines_Pruebas/Boletin_8/src/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
     public static void main(String[] args) {
         String texto="Isoo e Java!";
@@ -28,8 +30,34 @@ public class Main {
         String textoJ1="ABCD";
         System.out.println(textoJ1.replace("ABCD","ASCII"));
 
+        //Darle a letras1,numeros,espacios la cantidad que haya en textoJ3 comentando
         String textoJ3="Ola, son alumno de DAM1, e son programador desde 2024";
-        System.out.println(textoJ3.length());
+
+        int letras1=0;
+
+        int numeros=0;
+
+        int espacios=0;
+
+        //Usando un bucle para contar las letras, numeros y espacios en el textoJ3
+        for (int i = 0; i < textoJ3.length(); i++) {
+            //Obtener el carácter en la posición i de textoJ3
+            char c = textoJ3.charAt(i);
+            if (Character.isLetter(c)) {
+                //Contar letras
+                letras1++;
+
+            } else if (Character.isDigit(c)) {
+                //Contar dígitos numericos
+                numeros++;
+            } else if (Character.isWhitespace(c)) {
+                //Contar espacios en blanco
+                espacios++;
+            }
+        }
+        System.out.println("Letras: "+letras1);
+        System.out.println("Numeros: "+numeros);
+        System.out.println("Espacios: "+espacios);
 
     }
 }

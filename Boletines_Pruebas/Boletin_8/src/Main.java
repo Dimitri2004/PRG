1999ecaccd2951f76692e02f373cccc054f6d381
Dimitri2004 <dapariciopereira@danielcastelao.org>
2025-01-17 04:57:10 -1200

    Update Main.java

--- Boletines_Pruebas/Boletin_8/src/Main.java
+++ Boletines_Pruebas/Boletin_8/src/Main.java
@@ -2,7 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        //creamos el string del que vamos a trabajar
         String texto="Isoo e Java!";
+        //damos el valor numerico a la longitud de la frase
         System.out.println(texto.length());
         String textoJava="Java";
         String[] letras=textoJava.split("");
@@ -10,8 +12,9 @@ public class Main {
             System.out.println(letra);
         }
         String[] textoJava1="www. java,dende0 .com".split(",");
+        //funcion que divide nuestra frase que tenemos en el array de  string de arriba
         System.out.println(textoJava1[0]); //www. java
-        System.out.println(textoJava1[1]); //dende0.com
+        System.out.println(textoJava1[1]); //dende0.com 
 //        String[] partes= textoJava1.split("-");
 //        String parte1=partes[0];
 //        String parte2=partes[1];
@@ -33,15 +36,15 @@ public class Main {
         //Darle a letras1,numeros,espacios la cantidad que haya en textoJ3 comentando
         String textoJ3="Ola, son alumno de DAM1, e son programador desde 2024";
 
-        int letras1=0;
+        int letras1=0; //valor del espacio que ocupa
 
-        int numeros=0;
+        int numeros=0; //valores de la posicion de los numeros
 
-        int espacios=0;
+        int espacios=0; //valor de los espacions que tiene la frase
 
         //Usando un bucle para contar las letras, numeros y espacios en el textoJ3
         for (int i = 0; i < textoJ3.length(); i++) {
-            //Obtener el carácter en la posición i de textoJ3
+            //Obtener el carácter en la posición i de texto J3
             char c = textoJ3.charAt(i);
             if (Character.isLetter(c)) {
                 //Contar letras
@@ -55,8 +58,8 @@ public class Main {
                 espacios++;
             }
         }
-        System.out.println("Letras: "+letras1);
-        System.out.println("Numeros: "+numeros);
+        System.out.println("Letras: "+letras1);//sacalas letras que tiene la frase
+        System.out.println("Numeros: "+numeros);//saca los numeros 
         System.out.println("Espacios: "+espacios);
 
     }

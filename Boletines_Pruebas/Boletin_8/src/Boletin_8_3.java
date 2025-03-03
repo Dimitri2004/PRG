a6d93dc7f828d082beb2e73945b510469ca153f5
Dimitri2004 <dapariciopereira@danielcastelao.org>
2025-03-03 07:31:16 -1200

    Update Boletin_8_3.java

--- Boletines_Pruebas/Boletin_8/src/Boletin_8_3.java
+++ Boletines_Pruebas/Boletin_8/src/Boletin_8_3.java
@@ -6,21 +6,32 @@ import java.util.Scanner;
  */
 public class Boletin_8_3 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
+               
         System.out.print("Introduce una palabra: ");
+        
         String palabra = sc.nextLine();
+        
         int n = 1;
+        //Dividimos las palabras en n numeros
         String[] palabrasDividas = dividirPalabra(palabra, n);
+        
         System.out.println("Palabras divididas:");
+        
         Arrays.stream(palabrasDividas).forEach(System.out::println);
 
         sc.close();
 
     }
     public static String[] dividirPalabra(String palabra, int n) {
+        
         String[] palabrasDividas = new String[palabra.length()];
+        
         for ( int i = 0; i < palabra.length(); i++) {
-            palabrasDividas[i] = palabra.substring(i, Math.min(i*(n+1), palabra.length()));
+            
+            palabrasDividas[i] = palabra.substring(++1,i, Math.min(i*(n+1), palabra.length()));
+            
             i++;
         }
         return palabrasDividas;

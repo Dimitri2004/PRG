acf9fc7cc474866419ab965686aa31eae23ff6f3
Dima <dapariciopereira@danielcastelao.org>
2025-03-25 09:04:39 +0100

    Empiezo con el uso de JFrames

--- Boletines_Pruebas/Boletin_10/src/Strings/CorregirFrase.java
+++ Boletines_Pruebas/Boletin_10/src/Strings/CorregirFrase.java
@@ -7,17 +7,17 @@ public class CorregirFrase {
         System.out.println("La frase corregida es: " + corregirFrase(frase));
     }
 
-    //funcion que realiza la comprobacion
+    //funcion que realiza la comprobacion del cambio de la primera letra de cada palabra a mayuscula
     public static String corregirFrase(String frase){
         String fraseCorregida = "";
-        for (int i = 0; i < frase.length(); i++) {
-            if(Character.isUpperCase(frase.charAt(i))){
-                fraseCorregida += " " + frase.charAt(i);
-            }else{
-                fraseCorregida += frase.charAt(i);
+        String[] palabras = frase.split("(?=[A-Z])");
+        for (int i = 0; i < palabras.length; i++) {
+            fraseCorregida += palabras[i].substring(0, 1).toUpperCase() + palabras[i].substring(1).toLowerCase();
+            if(i < palabras.length - 1){
+                fraseCorregida += " ";
             }
-
         }
         return fraseCorregida;
     }
+
 }

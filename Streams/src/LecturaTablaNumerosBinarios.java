0334db93a153d7740bb41ae08f5428cc1c45c98d
Dima <dapariciopereira@danielcastelao.org>
2025-04-07 14:05:11 +0200

    Practica de inputs y outpus de ficheros de numeros inarios 0.3

--- Streams/src/LecturaTablaNumerosBinarios.java
+++ Streams/src/LecturaTablaNumerosBinarios.java
@@ -10,11 +10,11 @@ public class LecturaTablaNumerosBinarios {
                 System.out.println(flujoentrada.readInt());
            }
         } catch (FileNotFoundException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error no encontrado " + e.getMessage());
         } catch (EOFException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error sin datos " + e.getMessage());
         } catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error  " + e.getMessage());
         }finally {
             try {
                 if (flujoentrada != null) {

8722b40656eb8d0e09b016aa4b234c5820972c06
Dima <dapariciopereira@danielcastelao.org>
2025-04-07 14:01:00 +0200

    Practica de inputs y outpus de ficheros de numeros inarios 0.2

--- Streams/src/LecturaTablaNumerosBinarios.java
+++ Streams/src/LecturaTablaNumerosBinarios.java
@@ -1,13 +1,28 @@
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import javax.imageio.IIOException;
+import java.io.*;
 
 public class LecturaTablaNumerosBinarios {
     public LecturaTablaNumerosBinarios (String rutafFichero){
         ObjectInputStream flujoentrada = null;
         try {
-            flujoentrada = new ObjectInputStream(new FileInputStream("numeros.data"));
-            //read int
-
+            flujoentrada = new ObjectInputStream(new FileInputStream(rutafFichero));
+            while (true){
+                System.out.println(flujoentrada.readInt());
+           }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            try {
+                if (flujoentrada != null) {
+                    flujoentrada.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error cerrando archivo: " + e.getMessage());
+            }
         }
     }
 
--- Streams/src/Mani.java
+++ Streams/src/Mani.java
@@ -7,6 +7,8 @@ public class Mani {
         int[] tabla = {1, 2, 3, 4, 5,5,6,7,8};
 
         new EstructuraTablaNumerosBinarios(tabla);
+        new LecturaTablaNumerosBinarios("numeros.data");
+
 
     }
 }

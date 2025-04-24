d9f1abf759edc7144976cb61cf8af7899d6af5b6
Dima <dapariciopereira@danielcastelao.org>
2025-04-24 14:42:43 +0200

    Collections Empleo de objetos cliente

---
+++ Boletines_Pruebas/Coleccions_PR/Practicas/src/Cliente.java
@@ -0,0 +1,83 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Aprender a usar la clase LocalDate, DateTimeFormatter y la interfaz Comparable(en este caso utilizamos por ejemplo el compareTo)
+ * @author Oliver Miguez Alonso
+ */
+public class Cliente implements Comparable<Cliente> {
+    String dni;
+    String nome;
+    LocalDate dataNacemento; //clase para trabajar con datas
+
+    //CONSTRUCTOR
+    public Cliente(String dni, String nome, String dataNacemento){
+        this.dni = dni;
+        this.nome = nome;
+        /*
+        La clase DateTimeFormatter permite crear un formato de estructura para una data o fecha
+        y utilizando el metodo ofPattern es como le damos un formato sencillo siguiendo cierto patrón(especificado en la documentación)
+         */
+        DateTimeFormatter formatoDatas = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //Proporciona un formato específico a la data que introduciremos
+        /*
+        El utilizar el metodo parse lo que hacemos es darle a la propiedad "dataNacemento" el formato creado anteriormente
+         */
+        this.dataNacemento = LocalDate.parse(dataNacemento,formatoDatas);//a la "dataNacemento" se le aplicará el forma creando anteriormente
+    }//end constructor
+
+    /**
+     * Aquí se utiliza el metodo until de la clase LocalDate. Este metodo calcula la diferencia entre dos fechas,
+     * la fecha de nacimiento (dataNacemento) y la fecha actual (LocalDate.now()), usando la unidad de tiempo especificada.
+     * ChronoUnit.YEARS: Este es el tipo de unidad de tiempo que se pasa a until, lo que indica que queremos calcular la diferencia en años.
+     *
+     * Conversión a int: Aunque el metodo until devuelve un valor de tipo long, se hace un "cast" explícito a int porque
+     * la edad suele representarse como un número entero.
+     * @return la edad del cliente
+     */
+    int edade(){
+        return (int) dataNacemento.until(LocalDate.now(),ChronoUnit.YEARS); //representa la edad del cliente, gracias a calcular la diferencia entre la data actual con la data de nacimiento
+    }
+
+    /**
+     * Compara dos clientes, si por ejemplo el cliente anteriormente creado tiene una edad mayor mostrará una cosa con respecto a si tiene la misma edad
+     * o una edad menor.
+     * En coso de que sea un dni el compareTo lo verificará de manera distinta debido a que es un String
+     * @param outro cliente nuevo con el que se comparará
+     * @return
+     */
+    @Override
+    public int compareTo(Cliente outro) {
+
+        return this.edade()- outro.edade(); //si la edad de this.edad > outro.edad devuelve un valor positivo, en caso contrario negativo y en empate 0
+
+        /*
+       Otra Forma de hacerlo
+       int res = 0;
+       if(this.edade() > outro.edade()) res = 1;
+       if(this.edade() < outro.edade()) res = -1;
+
+       return res;
+        */
+
+    }
+
+    /**
+     * Muestra los datos por pantalla
+     * @return todos los datos de la clase Cliente
+     */
+    public String toString(){
+        return "DNI:"+ dni+" Nome:"+ nome + " Data de nacemento :"+ dataNacemento.toString();
+        /*
+        Usamos el toString predeterminado de dataNacemento que proviene de la clase LocalDate.
+        Este mostrará la fecha en formato ISO 8601 (YYYY-MM-DD) porque el formato creado en el constructor
+         solo se aplica al momento de inicializar la fecha y no afecta el comportamiento de este metodo.
+
+         Otro forma de hacerlo para aplicarle el formato deseado:
+         DateTimeFormatter formatoDatas = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+         return "DNI:" + dni + " Nome:" + nome + " Data de nacemento:" + dataNacemento.format(formatoDatas);
+
+        */
+    }
+
+}//end class
---
+++ Boletines_Pruebas/Coleccions_PR/Practicas/src/TratamentodeCliente.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Aprendiendo ArrayList
+ * @author Oliver Miguez Alonso
+ */
+public class TratamentodeCliente {
+    public static void main(String[] args) {
+        //De momento solo lo declaramos como collection y no como list
+        Collection<Cliente> coleccionCliente;
+        //creamos un arrayList
+        coleccionCliente = new ArrayList<>();
+        //Añade al array 3 nuevos clientes, ya que la lista solo permite añadir objetos formato "CLIENTE"
+        //el add devuelve true si se añadió o se modificó el tamaño del array a la lista y falso si la lista se mantiene igual y no cambia es decir si no se añade
+        coleccionCliente.add(new Cliente("00000000T","Oliver","03/06/2006"));
+        coleccionCliente.add(new Cliente("12345678Y","Pepe","02/05/2005"));
+        coleccionCliente.add(new Cliente("87654321R","Arturo","01/04/2004"));
+
+        //creamos un nuevo cliente
+        Cliente unCliente = new Cliente("77777777W","Jaime","06/11/2000");
+        coleccionCliente.add(unCliente);//lo añadimos al arrayList
+
+        //dispone de un toString
+        System.out.println(coleccionCliente.toString());
+        //Muestra el total de objetos que tiene la colección
+        System.out.println(coleccionCliente.size());
+        //Muestra si está o no vacía
+        System.out.println(coleccionCliente.isEmpty());
+        //Verifica si contiene un determinado elemento
+        System.out.println(coleccionCliente.contains(unCliente));
+        //elimina el cliente creado que fue añadido al arrayList
+        coleccionCliente.remove(unCliente);
+        //elimina cualquier elemento que tenga el ArrayList
+        coleccionCliente.clear();
+        //Referencia a cada elemento del array
+        Object[] clientes = coleccionCliente.toArray();
+        Iterator<Cliente> indice= coleccionCliente.iterator();
+
+    }
+}//end class
